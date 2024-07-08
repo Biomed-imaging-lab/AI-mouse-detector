98b31b217ea39076200928f144434987b9094839
slavikfish03 <slavikfish03@yandex.ru>
2024-07-09 00:46:56 +0300

    [fix](mouse_detector.py): fixed bugs

--- mouse_detector.py
+++ mouse_detector.py
@@ -7,11 +7,14 @@ from analytic_image_processor import AnalyticImageProcessor
 class MouseDetector:
     def __init__(self, path_to_video: str, path_to_weight_yolo: str, do_output_video: bool = False):
         self.input_video = cv2.VideoCapture(path_to_video)
+        if not self.input_video.isOpened():
+            self.input_video.release()
+            raise ValueError(f"[ERROR]: Couldn't open the video {path_to_video}")
         self.model = YOLO(path_to_weight_yolo)
         self.do_output_video = do_output_video
 
-    def search_center_and_zones(self, video: cv2.VideoCapture) -> dict:
-        _, frame = video.read()
+    def search_center_and_zones(self) -> dict:
+        _, frame = self.input_video.read()
         frame_analyzer = AnalyticImageProcessor(frame)
         frame_analyzer.search_zones()
         info_arena = {
@@ -45,14 +48,14 @@ class MouseDetector:
 
     def detect(self):
         if self.do_output_video:
-            output_video = cv2.VideoWriter('output_video_2.mp4',
+            output_video = cv2.VideoWriter('output_video.mp4',
                                            cv2.VideoWriter_fourcc(*'mp4v'),
                                            self.input_video.get(cv2.CAP_PROP_FPS),
                                            (int(self.input_video.get(cv2.CAP_PROP_FRAME_WIDTH)),
                                             int(self.input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
-        info_arena = self.search_center_and_zones(self.input_video)
-        
+        info_arena = self.search_center_and_zones()
+
         while True:
             ret, frame = self.input_video.read()
             if not ret:
@@ -77,22 +80,23 @@ class MouseDetector:
         frame = cv2.circle(frame, (info_arena['x_center'], info_arena['y_center']), info_arena['middle_zone'][1], (255, 0, 0), 2)
         frame = cv2.circle(frame, (info_arena['x_center'], info_arena['y_center']), info_arena['outer_zone'][1], (255, 0, 255), 2)
 
-        frame = cv2.line(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), (info_mouse['point_near'][0], info_mouse['point_near'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), (info_mouse['point_near'][0], info_mouse['point_near'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), (0, 255, 255), 2)
-        frame = cv2.line(frame, (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), (0, 255, 255), 2)
+        if len(info_mouse) != 0:
+            frame = cv2.line(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), (info_mouse['point_near'][0], info_mouse['point_near'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), (info_mouse['point_near'][0], info_mouse['point_near'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), (0, 255, 255), 2)
+            frame = cv2.line(frame, (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), (0, 255, 255), 2)
 
-        frame = cv2.circle(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), 3, (0, 0, 255), -1)
-        frame = cv2.circle(frame, (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), 3, (0, 255, 0), -1)
-        frame = cv2.circle(frame, (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), 3, (0, 255, 0), -1)
-        frame = cv2.circle(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), 3, (0, 0, 255), -1)
-        frame = cv2.circle(frame, (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), 3, (255, 0, 0), -1)
-        frame = cv2.circle(frame, (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), 3, (255, 0, 0), -1)
-        frame = cv2.circle(frame, (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), 3, (0, 0, 255), -1)
+            frame = cv2.circle(frame, (info_mouse['point_nose'][0], info_mouse['point_nose'][1]), 3, (0, 0, 255), -1)
+            frame = cv2.circle(frame, (info_mouse['point_r_ear'][0], info_mouse['point_r_ear'][1]), 3, (0, 255, 0), -1)
+            frame = cv2.circle(frame, (info_mouse['point_l_ear'][0], info_mouse['point_l_ear'][1]), 3, (0, 255, 0), -1)
+            frame = cv2.circle(frame, (info_mouse['point_near'][0], info_mouse['point_near'][1]), 3, (0, 0, 255), -1)
+            frame = cv2.circle(frame, (info_mouse['point_r_side'][0], info_mouse['point_r_side'][1]), 3, (255, 0, 0), -1)
+            frame = cv2.circle(frame, (info_mouse['point_l_side'][0], info_mouse['point_l_side'][1]), 3, (255, 0, 0), -1)
+            frame = cv2.circle(frame, (info_mouse['point_tail'][0], info_mouse['point_tail'][1]), 3, (0, 0, 255), -1)
 
         return frame
 
