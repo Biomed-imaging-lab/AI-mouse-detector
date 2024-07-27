[fix](mouse_detector.py): fix frame time calculation

mouse_detector.py
@@ -38,8 +38,6 @@ class MouseDetector:
             'middle_zone': frame_analyzer.middle_zone,
             'outer_zone': frame_analyzer.outer_zone
         }
-        # frame_analyzer.draw_zones(frame_analyzer.image)
-        # frame_analyzer.show()
         return info_arena
 
     def search_mouse(self, image: np.ndarray) -> dict:
@@ -98,14 +96,16 @@ class MouseDetector:
     def get_time_frame(self, frame_number: int, fps: float):
         current_time_seconds = frame_number / fps
         minutes, seconds = divmod(current_time_seconds, 60)
-        return minutes, seconds
+        minutes_str = f'{int(minutes):02d}'
+        seconds_str = f'{seconds:05.2f}'.replace('.', ',')
+        return minutes_str, seconds_str
 
     def get_name_output_csv(self, path_to_video: str):
         output_filename = os.path.basename(path_to_video)
         name = output_filename.split('.')[0]
         return name
 
-    def calculate(self, info_mouse: dict, info_arena: dict, time_frame: tuple[float, float]) -> List:
+    def calculate(self, info_mouse: dict, info_arena: dict, time_frame: tuple[str, str]) -> List:
         calculator = Calculator(info_mouse, info_arena, time_frame)
         return calculator.calculate()
 