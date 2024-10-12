70cf47c317d02265e9ace894f681559eb9b51bc5
slavikfish03 <slavikfish03@yandex.ru>
2024-10-12 15:17:24 +0300

    [fix]: fix shift

--- behavior_analyzer.py
+++ behavior_analyzer.py
@@ -17,7 +17,7 @@ class BehaviorAnalyzer:
         self.height = height
         self.width = width
         self.model = YOLO(path_to_behavior_weight_yolo)
-        self.shift = None
+        self.shift = COUNT_FRAMES_IN_COMPOSITE_IMG // 2
 
         self.output_name_csv = self.get_name_output_csv(path_to_video)
         self.init_csv_file()
@@ -30,10 +30,6 @@ class BehaviorAnalyzer:
             writer = csv.writer(file)
             writer.writerow(row)
 
-
-    def set_shift(self, frame_number):
-        if self.shift is None:
-            self.shift = frame_number
 
     def create_composite_frame(self):
         composite_img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
